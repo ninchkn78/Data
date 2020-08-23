4e7c87d96b488d3962b23d8193b9c9067d6fcc56
Alex Chao <ac590@duke.edu>
2020-08-23 14:46:15 -0500

    added methods to output answers to questions 1 and 2

--- src/names/Output.java
+++ src/names/Output.java
@@ -12,7 +12,20 @@ public class Output {
         Process process = new Process(year);
         String MaleName = process.TopName("M");
         String FemaleName = process.TopName("F");
-        return "Top Female Name is: " + FemaleName + "\nTop Male Name is: " + MaleName;
+        return "F: " + FemaleName + "\nM: " + MaleName;
+    }
+    public String CountBabiesForYearGenderFirstLetter(int year, char letter, String gender){
+        Process process = new Process(year);
+        int CountNames = process.NamesStartWith(letter, gender);
+        int TotalBabies = process.TotalCount(letter,gender);
+        return "#OfNames: " + Integer.toString(CountNames) + "\n#OfBabies: " + Integer.toString(TotalBabies);
+    }
+    public static void main(String[] args)
+    {
+        Output Test = new Output();
+        System.out.println(Test.CountBabiesForYearGenderFirstLetter(1900,'Q',"M"));
+        System.out.println(Test.CountBabiesForYearGenderFirstLetter(1900,'Q',"F"));
+        System.out.println(Test.TopRankedNamesForYear(1900));
 
     }
 }
