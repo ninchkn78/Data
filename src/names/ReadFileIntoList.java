93db31867da1a8ad481c399e3ad59598e283363c
Alex Chao <ac590@duke.edu>
2020-08-24 15:45:11 -0500

    updated ReadFileIntoList to use paths instead of File

--- src/names/ReadFileIntoList.java
+++ src/names/ReadFileIntoList.java
@@ -1,49 +1,52 @@
 package names;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ReadFileIntoList {
+    private String FileName;
+
     //TO-DO: generalize this to be able to handle URL or different file locations/naming conventions
-    private String generateDataSource(int year){
-        String FileName = "yob" + year + ".txt";
-        String PathName = "C:/Users/alexc/CS307/data_team01/data/ssa_complete/";
-        return PathName + FileName;
+    private void getFileName(int year){
+        FileName = "yob" + year + ".txt";
     }
     //change data structure later
     public List<String[]> generateList(int year) {
-        // pass the path to the file as a parameter
-        File file =
-                new File(generateDataSource(year));
         List<String[]> profiles = new ArrayList<>();
         String[] profile;
+
+        getFileName(year);
+
         try {
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            String st;
-            while ((st = br.readLine()) != null) {
-                profile = st.split(",");
+            Path path = Paths.get(ReadFileIntoList.class.getClassLoader().getResource(FileName).toURI());
+            for (String line : Files.readAllLines(path)) {
+                profile = line.split(",");
                 profiles.add(profile);
             }
             return profiles;
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
     }
+        catch (URISyntaxException e){
+            e.printStackTrace();
+        }
         return profiles;
     }
-   /* public static void main(String[] args) throws Exception
+    /*
+   public static void main(String[] args) throws Exception
     {
         ReadFileIntoList names = new ReadFileIntoList();
         System.out.println(names.generateList(1900).get(0)[1]);
     }
 
-    */
+     */
+
+
 }
 
 
